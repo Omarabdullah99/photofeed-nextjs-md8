eef3794f735f6b295e54b490bb1837369c58540d
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-12 23:56:56 +0600

    photo details logic done and also apply language

---
+++ app/[lang]/photos/[id]/page.js
@@ -0,0 +1,11 @@
+import PhotoDetails from "@/components/PhotoDetails"
+
+
+const PhotoDetailsPage = ({params: {id, lang}}) => {
+
+    return (
+        <PhotoDetails id={id} lang={lang} />
+    )
+}
+
+export default PhotoDetailsPage
