4c286c1ddd8e5f1a4a53c7548f5b413602bc22e6
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-12 19:54:29 +0600

    International/language logic setup done

---
+++ app/[lang]/page.js
@@ -0,0 +1,9 @@
+import { getDictionary } from "./disctionaries";
+
+
+export default async function Home({params: {lang}}) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <div>{dictionary.followers}</div>
+  );
+}
